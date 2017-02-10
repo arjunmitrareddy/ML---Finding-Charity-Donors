d8b7fc1b186a63e54f273404dd563cf4856ecba9
arjunmitrareddy <c.arjun.donthala@uptake.com>
2017-02-09 21:16:28 -0600

    feat(project): full project code

--- donors.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-from time import time
-
-data = pd.read_csv("census.csv")
-
-# Success - Display the first record
-#print(data.head(n=1))
-print(len(data.loc[data['income'] == '<=50K']))
